a5bcc02e022a922654fc8b6d8770961b1fedad78
achingbrain <alex@achingbrain.net>
2023-02-07 08:54:14 +0100

    feat: initial implementation

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * An object that contains an AbortSignal as
+ * the optional `signal` property.
+ *
+ * @example
+ *
+ * ```js
+ * const controller = new AbortController()
+ *
+ * aLongRunningOperation({
+ *   signal: controller.signal
+ * })
+ *
+ * // later
+ *
+ * controller.abort()
+ */
+export interface AbortOptions {
+  signal?: AbortSignal
+}
